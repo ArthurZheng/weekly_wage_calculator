e41f6a36d3c26b52c0de8cbdd7bd8b89a1c359d5
Jun Zheng <arthurjordan@gmail.com>
2015-07-03 22:06:13 +1000

    awesome initial commit

---
+++ weekly_wage_calculator/weekly_wage_calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  weekly_wage_calculator
+//
+//  Created by Andrew Spinks on 3/07/2015.
+//  Copyright (c) 2015 Techtransformers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
